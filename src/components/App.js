71fb24b36a4a43b9e32feab0a13a4e53a69a23e3
imhsart <nitinrana2519@gmail.com>
2026-05-18 16:00:48 +0530

    Assignment attempt 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,41 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, {useState} from 'react'
 
+const App = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState({userError:'', passError:''})
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    setTimeout(() => {
+      if(email !== 'admin12@gmail.com'){
+      setError({userError: 'User Not Found', passError: ''})
+      return
+      }
+      if(password !== '12345'){
+        setError({userError:'', passError: 'Password Incorrect'})
+        return
+      }
+      if(email === 'admin12@gmail.com' && password === '12345'){
+        alert('Logged In Successfully!')
+        setError({userError:'', passError:''})
+        setPassword('')
+        setEmail('')
+      }
+    }, 3000)
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input id='input-email' value={email} type='email' onChange={e => setEmail(e.target.value)}></input>
+        <p id='user-error'>{error.userError}</p>
+        <input id='input-password' value={password} type='password' onChange={e => setPassword(e.target.value)}></input>
+        <p id='password-error'>{error.passError}</p>
+        <button id='submit-form-btn' type='submit'>Log In</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
