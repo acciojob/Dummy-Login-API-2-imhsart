ad3225c2b2857a4064c2201b4991ff7e41ba5e13
imhsart <nitinrana2519@gmail.com>
2026-05-18 16:31:19 +0530

    Assignment attempt 4

--- src/components/App.js
+++ src/components/App.js
@@ -7,29 +7,33 @@ const App = () => {
 
   function handleSubmit(e){
     e.preventDefault()
-    if(email === '' || email !== 'admin12@gmail.com'){
-      setError({userError: 'User not found', passError: ''})
-      return
-    }
-    if(password === '' || password !== '12345'){
-      setError({userError:'', passError: 'Password Incorrect'})
-      return
-    }
-    if(email === 'admin12@gmail.com' && password === '12345'){
       setTimeout(() => {
-        alert('Logged In Successfully!')
+        if(email === '' || email !== 'admin12@gmail.com'){
+          setError({userError: 'User not found', passError: ''})
+          return
+        }
+        if(password === '' || password !== '12345'){
+          setError({userError:'', passError: 'Password Incorrect'})
+          return
+        }
+        console.log('Logged In Successfully!')
         setError({userError:'', passError:''})
         setPassword('')
         setEmail('')
-       }, 3000)
-    }
+    }, 3000)
   }
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input id='input-email' value={email} type='email' onChange={e => setEmail(e.target.value)}></input>
+        <input id='input-email' value={email} type='email' onChange={e => {
+          setEmail(e.target.value)
+          setError({userError:'', passError:''})
+          }}></input>
         <p id='user-error' style={{color:'red'}}>{error.userError}</p>
-        <input id='input-password' value={password} type='password' onChange={e => setPassword(e.target.value)}></input>
+        <input id='input-password' value={password} type='password' onChange={e => {
+          setPassword(e.target.value)
+          setError({userError:'', passError:''})
+          }}></input>
         <p id='password-error' style={{color:'red'}}>{error.passError}</p>
         <button id='submit-form-btn' type='submit'>Log In</button>
       </form>
