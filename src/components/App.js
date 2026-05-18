4de423f4b5515ebf6bb256aef69be3a0dd737789
imhsart <nitinrana2519@gmail.com>
2026-05-18 16:12:49 +0530

    Assignment attempt 3

--- src/components/App.js
+++ src/components/App.js
@@ -7,31 +7,30 @@ const App = () => {
 
   function handleSubmit(e){
     e.preventDefault()
-
-    setTimeout(() => {
-      if(email !== 'admin12@gmail.com'){
+    if(email === '' || email !== 'admin12@gmail.com'){
       setError({userError: 'User not found', passError: ''})
       return
-      }
-      if(password !== '12345'){
-        setError({userError:'', passError: 'Password Incorrect'})
-        return
-      }
-      if(email === 'admin12@gmail.com' && password === '12345'){
+    }
+    if(password === '' || password !== '12345'){
+      setError({userError:'', passError: 'Password Incorrect'})
+      return
+    }
+    if(email === 'admin12@gmail.com' && password === '12345'){
+      setTimeout(() => {
         alert('Logged In Successfully!')
         setError({userError:'', passError:''})
         setPassword('')
         setEmail('')
-      }
-    }, 3000)
+       }, 3000)
+    }
   }
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <input id='input-email' value={email} type='email' onChange={e => setEmail(e.target.value)}></input>
-        <p id='user-error'>{error.userError}</p>
+        <p id='user-error' style={{color:'red'}}>{error.userError}</p>
         <input id='input-password' value={password} type='password' onChange={e => setPassword(e.target.value)}></input>
-        <p id='password-error'>{error.passError}</p>
+        <p id='password-error' style={{color:'red'}}>{error.passError}</p>
         <button id='submit-form-btn' type='submit'>Log In</button>
       </form>
     </div>
